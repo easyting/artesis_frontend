ba8491ebf681a86b9727ccd10ceb27e35df4752e
Alex Oroshchuk <ao@inlead.dk>
2012-09-21 18:20:57 +0300

    case 915 - Moved artesis-specific code to artesis_frontend.

---
+++ js/ipe_quick_add.js
@@ -0,0 +1,18 @@
+(function ($) {
+  $('.ipe-trigger').live('click', function(e) {
+    var links = '#ipe-add-' + $(this).attr('target_region');
+    var options = {
+      'my': 'left top',
+      'at': 'left bottom',
+      'of': $(this),
+      'offset': '0 3'
+    };
+    $(links).fadeToggle('fast').position(options);
+//    $(this).addClass();
+    return false;
+  });
+
+  $('.ipe-popup').live('mouseleave', function() {
+    $(this).fadeToggle('fast');
+  });
+})(jQuery);
