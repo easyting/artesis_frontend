3cfab315c14f7fc14008e0fa269ab4c70d32b44d
Alex Oroshchuk <ao@inlead.dk>
2012-09-25 13:49:43 +0300

    case 915 - Styles changed in order to use position functionality.

--- js/ipe.js
+++ js/ipe.js
@@ -5,18 +5,7 @@
       'my': 'left top',
       'at': 'left bottom',
       'of': $(this),
-      'offset': '0 3',
-      // Use custom positionning function since 'collision' doesn't work as expected.
-      'using': function(pos) {
-        var right_side = $(this).parent().offset().left + pos.left + ipe_popup_width;
-        if (right_side > $(window).width()) {
-          var new_left = pos.left - (right_side - $(window).width()) - 20; // Correction applied
-          $(this).css({left: new_left, top: pos.top});
-        }
-        else {
-          $(this).css({left: pos.left, top: pos.top});
-        }
-      }
+      'offset': '0 3'
     };
     $(links).fadeToggle('fast').position(options);
     return false;
