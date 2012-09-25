5c32cf7f3dc8617a54e8ce40461027041dcc275d
Alex Oroshchuk <ao@inlead.dk>
2012-09-25 12:12:32 +0300

    case 915 - Fix positioning when outside the window.

--- js/ipe.js
+++ js/ipe.js
@@ -5,7 +5,18 @@
       'my': 'left top',
       'at': 'left bottom',
       'of': $(this),
-      'offset': '0 3'
+      'offset': '0 3',
+      // Use custom positionning function since 'collision' doesn't work as expected.
+      'using': function(pos) {
+        var right_side = $(this).parent().offset().left + pos.left + ipe_popup_width;
+        if (right_side > $(window).width()) {
+          var new_left = pos.left - (right_side - $(window).width()) - 20; // Correction applied
+          $(this).css({left: new_left, top: pos.top});
+        }
+        else {
+          $(this).css({left: pos.left, top: pos.top});
+        }
+      }
     };
     $(links).fadeToggle('fast').position(options);
     return false;
